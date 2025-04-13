35f846f2dae773a4c7d31b1e0237be8cc60cd787
STORKDev <jufoli@gmail.com>
2025-04-14 00:31:54 +0300

    added info messages
    .reg file doesn't exists and job's done messages added

--- powershell-script.ps1
+++ powershell-script.ps1
@@ -13,6 +13,13 @@ if ($fileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
     # Define the path of the .reg file to modify
     $regFilePath = "magnet_torrent_associate_utorrent.reg"
 
+    # Check if the .reg file exists
+    if (-Not (Test-Path $regFilePath)) {
+        # Show an error message box if the file doesn't exist
+        [System.Windows.Forms.MessageBox]::Show("magnet_torrent_associate_utorrent.reg file does not exist! Download from https://github.com/warshtranker/utorrent_magnet_fix", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+        return
+    }
+
     # Read the contents of the .reg file
     $regFileContent = Get-Content $regFilePath
 
@@ -29,6 +36,10 @@ if ($fileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
     Set-Content -Path $regFilePath -Value $updatedRegFileContent
 
     Write-Host "Registry file updated successfully. New path: $newPath"
+
+    # Show a message box indicating the job is done
+    [System.Windows.Forms.MessageBox]::Show("magnet_torrent_associate_utorrent.reg file updated successfully. New path is: $newPath", "Job's Done", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+
 } else {
     Write-Host "No file was selected."
 }
